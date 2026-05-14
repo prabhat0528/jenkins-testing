53d66ebab836a7a77d9617a894c484d66c9f67af
prabhat0528 <prabhatrai0528@gmail.com>
2026-05-14 19:24:55 +0530

    Added webhook

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "This is home route"
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    print(request.data)
+    return "Webhook received", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=2002)
